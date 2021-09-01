7572aa624594ba0ad962a02af33cd1086098bcc0
yeasirarafatpp <yeasira79@gmail.com>
2021-09-02 01:26:29 +0600

    All Section Done

--- js/app.js
+++ js/app.js
@@ -11,22 +11,46 @@ const loadBook = () => {
 }
 // Display Book
 const displayBook = books => {
-    books.forEach(book => {
-        console.log(book);
-        const booksCard = document.getElementById('books');
-        // booksCard.textContent = '';
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
+    const booksCard = document.getElementById('books');
+    booksCard.textContent = '';
+    if (books.length === 0 || !books) {
+        const notFound = document.getElementById('not-found');
+        notFound.style.display = 'block';
+    }
+    else {
+        books.forEach(book => {
+            console.log(book);
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
                     <div class="card">
                         <div class="card-body">
                             <h5 class="card-title">${book.title}</h5>
                             <p>By ${book.author_name}</p>
-                            <p>First Publish ${book.publish_date}</p>
+                            <p>First Publish ${book.first_publish_year}</p>
                             <p>Publisher: ${book.publisher}</p>
                         </div>
                     </div>
             `;
-        booksCard.appendChild(div);
-    });
+            booksCard.appendChild(div);
+            const notFound = document.getElementById('not-found');
+            notFound.style.display = 'none';
+        });
+    }
+
 }
+
+
+// Count for show counter and not found result
+const loadCounter = () => {
+    const searchField = document.getElementById('search-text');
+    const searchText = searchField.value;
+    searchField.value = '';
+    // console.log(searchText);
+    const url = `http://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data.numFound));
+
+}
+loadCounter();
